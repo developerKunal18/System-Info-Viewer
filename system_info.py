e0b2cfa4786a10bc64a78b52fe6e56b23ebc84ed
kunal karbhari <fullstackdeveloperkunal18@gmail.com>
2026-03-30 23:05:16 +0530

    Create system_info.py

---
+++ system_info.py
@@ -0,0 +1,8 @@
+import platform
+
+print("System Information:\n")
+
+print("OS:", platform.system())
+print("Platform:", platform.platform())
+print("Processor:", platform.processor())
+print("Python Version:", platform.python_version())
